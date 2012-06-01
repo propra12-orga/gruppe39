4027babfde8bf02573da60eb4bc2a336feb8cf74
Stefan <rubbinnexx@Rubbl-Desktop>
2012-06-01 21:29:34 +0200

    >>> Spielfeld - code aufgeraeumt - vorbereitungen fuer auslesen aus einer xml datei getroffen
    >>> StateGameMode
    - kleines beispiel (unvollstaendig) fuer kollisionsabfrage
    - code soweit unabhaengig von klasse spielfeld (da level auslesen noch
    fehlt)

--- Bomberman/src/Spielfeld.java
+++ Bomberman/src/Spielfeld.java
@@ -4,206 +4,71 @@ import java.util.Vector;
 
 public class Spielfeld 
 {
-	// hoechstwahrscheinlich werden wir kein STATE f�r Spielfeld brauchen
-	public static enum STATE
-	{
-		NO_STATE,
-		GAME_RUNNING,
-		GAME_PAUSED,
-		GAME_OVER
-	};
-	
-	private STATE state = STATE.NO_STATE;
-	
-	/*
-	 * Ein K�stchen ist 32p breit und 32p hoch . 
-	 * => hat das Spielfeld insgesamt 15*13 K�stchen
-	 */
-	public final Entity[][] spielfeld = new Entity[15][13];
+	public Entity[][] Arena;
 	
 	
 	
 	// spielfeld inhalt ::: 
-	private Vector<BombermanEntity> bomberman_entities = new Vector<BombermanEntity>();
-	private Vector<BombeEntity> bombe_entities = new Vector <BombeEntity>();
-	private Vector<ExplosionEntity> explosion_entities = new Vector <ExplosionEntity>();
-	private Vector<UnbreakableEntity> unbreakable_entities = new Vector <UnbreakableEntity>();
-	private Vector<BreakableEntity> breakable_entities = new Vector <BreakableEntity>();
+	private Vector<BombermanEntity> EBomberman = new Vector<BombermanEntity>();
+	private Vector<BombeEntity> EBomb = new Vector <BombeEntity>();
+	private Vector<ExplosionEntity> EExplosion = new Vector <ExplosionEntity>();
+	private Vector<UnbreakableEntity> EUnbreakable = new Vector <UnbreakableEntity>();
+	private Vector<BreakableEntity> EBreakable = new Vector <BreakableEntity>();
 
-	
-	private void fillUnbreakable()
-	{
-		// ring of unbreakables
-		for (int i = 0; i < 15; i++)
-		{
-			Entity e = new UnbreakableEntity("Tile_UnBreakable",i * 32, 0);
-			unbreakable_entities.add((UnbreakableEntity) e);	// vector
-			this.spielfeld[i][0] = e;
-			
-			Entity e2 = new UnbreakableEntity("Tile_UnBreakable",i * 32, 32 * 12);
-			unbreakable_entities.add((UnbreakableEntity) e2);	// vector
-			this.spielfeld[i][12] = e2;
-		}
-		
-		for (int i = 1; i < 13; i++)
-		{
-			Entity e = new UnbreakableEntity("Tile_UnBreakable", 0, 32 * i);
-			unbreakable_entities.add((UnbreakableEntity) e);	// vector
-			this.spielfeld[0][i] = e;
-			
-			Entity e2 = new UnbreakableEntity("Tile_UnBreakable", 32 * 14, 32 * i);
-			unbreakable_entities.add((UnbreakableEntity) e2);	// vector
-			this.spielfeld[14][i] = e2;
-		}
-		
-	/*	
-		for (i =1 ; i<15; i+=2)
-		{
-			for (j=1; j<13; j+=2)
-			{
-				e = new UnbreakableEntity("Tile_UnBreakable",i*32, j*32);
-				unbreakable_entities.add((UnbreakableEntity) e);	// vector
-				this.spielfeld[i][j] = e;
-				
-			}
-		}
-		*/
-	}
-	
-	private void makeBomberman()
-	{
-		Entity e = new BombermanEntity("Bomberman", 32 * 1, 32 * 1);
-		bomberman_entities.add((BombermanEntity) e);
-		this.spielfeld[0][0] = e;
-	}
-	
-	private void makeBreakable()
-	{
-
-		int i,j;
-	    for (i=0; i<15; i+=2)
-	    {	
-			for (j=0; j<13; j+=2)
-	    	{
-				Entity e = new BreakableEntity("Tile_Breakable", i*32, j*32);
-				breakable_entities.add((BreakableEntity) e);
-				this.spielfeld[i][j] = e;
-	       }
-	    } 	
-
-	}    
-	
 	public Spielfeld()
 	{
-		fillUnbreakable();
-		makeBomberman();
-		//makeBreakable();
+		// TODO
+		// - level aus xml datei auslesen -> eigene xml parser klasse
+		// - xml parser klasse schreibt werte, anzahl, positionen der einzelnen objekte in eine klasse
+		// - Arena initialisieren: klasse mit werten aus tabelle an funktion hier uebergeben
 	}
-	
-	public void addEntitiesToScreen(Gamescreen cur_gamescreen)
-	{
-		for (int i = 0; i < this.unbreakable_entities.size(); i++)
-		{
-			cur_gamescreen.addEntityToScreen("unbreakable" + i, this.unbreakable_entities.get(i));
-		}
-		
-		for (int i = 0; i < this.breakable_entities.size(); i++)
-		{
-			cur_gamescreen.addEntityToScreen("breakable" + i, this.breakable_entities.get(i));
-		}
-		
-		for (int i = 0; i < this.bomberman_entities.size(); i++)
-		{
-			cur_gamescreen.addEntityToScreen("bomberman" + i, this.bomberman_entities.get(i));
-		}
 
-		// TODO: unbreakable
+	
+	public void addInitialArenaToScreen(Gamescreen cur_gamescreen)
+	{
+		for (int i = 0; i < this.EUnbreakable.size(); i++)
+		{
+			cur_gamescreen.addEntityToScreen("unbreakable" + i, this.EBreakable.get(i));
+		}
 		
+		for (int i = 0; i < this.EBreakable.size(); i++)
+		{
+			cur_gamescreen.addEntityToScreen("breakable" + i, this.EBreakable.get(i));
+		}
+		
+		for (int i = 0; i < this.EBomberman.size(); i++)
+		{
+			cur_gamescreen.addEntityToScreen("bomberman" + i, this.EBomberman.get(i));
+		}
 	}
 
 	
 	public void addBomb(int x, int y)
 	{
-		// TODO: bombe legen + status setzen + ticken lassen etc.
-		// bombe erzeugen...
-		//bombe_entities.add(bomb);
+		// TODO: 
+		// koordinaten pruefen (nicht ausserhalb vom spielfeld)
+		// bombenobjekt erzeugen
+		// spielfeld an bombe uebergeben, damit bombe nach beenden des tickvorgangs die explosionen erzeugen kann
+		// synchronisation fuer zugriff auf das spielfeld (wichtig!!!)		
 	}
 	
-	public void addExplosion(ExplosionEntity expl)
+	public void fillArena(/* parameter */)
 	{
-		// TODO: explosionen erzeugt die bombe in eigenem privaten thread
-		explosion_entities.add(expl);
+		// TODO 
+		// arbeite alle kategorien hier ab und fuelle das spielfeld
+		// entsprechend der werte, die aus der datei ausgelesen wurden
+		// fuelle hierzu nicht nur Arena, sondern auch die vektoren
 	}
 	
-	public void deleteEntity(Entity entity)
+	public void clearArena()
 	{
-		if (entity instanceof BombermanEntity)
+		for (int i = 0; i < this.Arena.length; i++)
 		{
-			// es gibt nur einen Bomberman
-			bomberman_entities.remove(entity);
-		}
-		else if (entity instanceof ExplosionEntity)
-		{
-			// man braucht also nur einen der Explosionen weg zu machen und alle anderen werden mitgel�scht.
-			explosion_entities.clear();
-		}
-		else if (entity instanceof BreakableEntity)
-		{
-			BreakableEntity br_e = (BreakableEntity) entity; 
-			
-			for (int i =0; i<breakable_entities.size(); i++)
-				if (breakable_entities.get(i) == br_e )
-				{
-					breakable_entities.remove(i);
-					break;
-				}
-		}
-		else
-		{
-			// do nothing 
+			for (int j = 0; j < this.Arena[i].length; j++)
+			{
+				this.Arena[i][j] = null;
+			}
 		}
 	}
-	
-    public Entity getEntity(int x, int y)
-    {
-    	// TODO:
-    	// abfrage ob x und y außerhalb vom spielfeld -> error
-    	// return this.spielfeld[x][y]
-    	int i;
-    	// wenn entity unbreakable
-    	if  ( ( ( (x/32) % 2 ) == 1 )  && ( ( (y/32) % 2 ) == 1 ) )
-    	{
-    		for (i=0; i<unbreakable_entities.size(); i++)
-    			if (	 (unbreakable_entities.get(i).getLocationX() == x)
-    				  && (unbreakable_entities.get(i).getLocationY() == y) )
-    				return unbreakable_entities.get(i);
-    	}
-    	else
-    	{
-    		Vector<BombermanEntity> bomberman_entities;
-		// gucken ob bomberman : es gibt nur einen deswegen get(0)
-    	for (i=0; i < this.bomberman_entities.size(); i++)	
-    		if ( (this.bomberman_entities.get(0).getLocationX() == x) && (this.bomberman_entities.get(0).getLocationY() == y) )
-    			return this.bomberman_entities.get(0);
-    		// es gibt auch nur einen bomb ; sp�ter vielleicht mit mehreren bombermen
-    	 for (i=0; i<this.bombe_entities.size(); i++)
-    		 if ( (this.bombe_entities.get(0).getLocationX() == x) && (this.bombe_entities.get(0).getLocationY() == y) )
-    			return this.bombe_entities.get(0);
-    		
-    		else
-    		{
-    			for (i=0; i<explosion_entities.size(); i++)
-    				if (  ( explosion_entities.get(i).getLocationX() == x ) && ( explosion_entities.get(i).getLocationY() == y)  )
-    					return explosion_entities.get(i);
-    			
-    			for (i=0; i<breakable_entities.size(); i++)
-    				if (  ( breakable_entities.get(i).getLocationX() == x) && (breakable_entities.get(i).getLocationY() == y) )
-    					return breakable_entities.get(i);
-    		}
-    		
-    	}
-    	
-    	return null;
-    }
     
 }
--- Bomberman/src/StateGameMode.java
+++ Bomberman/src/StateGameMode.java
@@ -1,6 +1,7 @@
 import java.awt.Graphics2D;
 
 
+
 /*********************************************************************
 * StateGameMode
 * desc: 	this state resembles our current running game, aka where 
@@ -9,6 +10,16 @@ import java.awt.Graphics2D;
 **************************************************************************/
 public class StateGameMode implements InterfaceState
 {
+	public static enum STATE
+	{
+		NO_STATE,
+		GAME_RUNNING,
+		GAME_PAUSED,
+		GAME_OVER
+	};
+	
+	private STATE state = STATE.NO_STATE;
+	
 	private Gamestate Cur_Gamestate;
 	private Gamescreen Cur_Gamescreen;
 	
@@ -20,7 +31,7 @@ public class StateGameMode implements InterfaceState
 	}
 	
 	//private Spielfeld spielfeld;
-	private final Spielfeld spielfeld  =  new Spielfeld();
+	public final Entity[][] spielfeld = new Entity[15][13];
 
 
 	@Override
@@ -29,8 +40,34 @@ public class StateGameMode implements InterfaceState
 		// init inputs for one player
 		KeyboardInput.init(1);
 		
-		// TODO Auto-generated method stub
-		spielfeld.addEntitiesToScreen(this.Cur_Gamescreen);	
+		// ring of unbreakables
+		for (int i = 0; i < 15; i++)
+		{
+			Entity e = new UnbreakableEntity("Tile_UnBreakable",i * 32, 0);
+			this.spielfeld[i][0] = e;
+			this.Cur_Gamescreen.addEntityToScreen("unbreak" + i, e);
+					
+			Entity e2 = new UnbreakableEntity("Tile_UnBreakable",i * 32, 32 * 12);
+			this.spielfeld[i][12] = e2;
+			this.Cur_Gamescreen.addEntityToScreen("unbreak" + i + 15, e2);
+		}
+				
+		for (int i = 1; i < 13; i++)
+		{
+			Entity e = new UnbreakableEntity("Tile_UnBreakable", 0, 32 * i);
+			this.Cur_Gamescreen.addEntityToScreen("unbreak" + 30 + i, e);
+			this.spielfeld[0][i] = e;
+					
+			Entity e2 = new UnbreakableEntity("Tile_UnBreakable", 32 * 14, 32 * i);
+			this.Cur_Gamescreen.addEntityToScreen("unbreak" + 30 + i + 13, e2);
+			this.spielfeld[14][i] = e2;
+		}
+		
+		// bomberman
+		Entity e = new BombermanEntity("Bomberman", 32 * 1, 32 * 1);
+		this.Cur_Gamescreen.addEntityToScreen("bomberman0", e);
+		this.spielfeld[1][1] = e;
+		
 	}
 
 	@Override
@@ -49,8 +86,10 @@ public class StateGameMode implements InterfaceState
 			
 			if (bomberman.isMoving() == false)
 			{
-				if (this.spielfeld.spielfeld[bomberman.getTileX()][bomberman.getTileY() + 1] == null)
+				if (this.spielfeld[bomberman.getTileX()][bomberman.getTileY() + 1] == null)
 				{
+					this.spielfeld[bomberman.getTileX()][bomberman.getTileY() + 1] = this.spielfeld[bomberman.getTileX()][bomberman.getTileY()];
+					this.spielfeld[bomberman.getTileX()][bomberman.getTileY()] = null;
 					this.Cur_Gamescreen.getEntity("bomberman0").moveTileY(1, 3);
 				}
 			}
@@ -62,8 +101,10 @@ public class StateGameMode implements InterfaceState
 			
 			if (bomberman.isMoving() == false)
 			{
-				if (this.spielfeld.spielfeld[bomberman.getTileX()][bomberman.getTileY() - 1] == null)
+				if (this.spielfeld[bomberman.getTileX()][bomberman.getTileY() - 1] == null)
 				{
+					this.spielfeld[bomberman.getTileX()][bomberman.getTileY() - 1] = this.spielfeld[bomberman.getTileX()][bomberman.getTileY()];
+					this.spielfeld[bomberman.getTileX()][bomberman.getTileY()] = null;
 					this.Cur_Gamescreen.getEntity("bomberman0").moveTileY(-1, 3);
 				}
 			}
@@ -74,8 +115,10 @@ public class StateGameMode implements InterfaceState
 			
 			if (bomberman.isMoving() == false)
 			{
-				if (this.spielfeld.spielfeld[bomberman.getTileX() - 1][bomberman.getTileY()] == null)
+				if (this.spielfeld[bomberman.getTileX() - 1][bomberman.getTileY()] == null)
 				{
+					this.spielfeld[bomberman.getTileX() - 1][bomberman.getTileY()] = this.spielfeld[bomberman.getTileX()][bomberman.getTileY()];
+					this.spielfeld[bomberman.getTileX()][bomberman.getTileY()] = null;
 					this.Cur_Gamescreen.getEntity("bomberman0").moveTileX(-1, 3);
 				}
 			}
@@ -87,8 +130,10 @@ public class StateGameMode implements InterfaceState
 			
 			if (bomberman.isMoving() == false)
 			{
-				if (this.spielfeld.spielfeld[bomberman.getTileX() + 1][bomberman.getTileY()] == null)
+				if (this.spielfeld[bomberman.getTileX() + 1][bomberman.getTileY()] == null)
 				{
+					this.spielfeld[bomberman.getTileX() + 1][bomberman.getTileY()] = this.spielfeld[bomberman.getTileX()][bomberman.getTileY()];
+					this.spielfeld[bomberman.getTileX()][bomberman.getTileY()] = null;
 					this.Cur_Gamescreen.getEntity("bomberman0").moveTileX(1, 3);
 				}
 			}
