ceecfc0592690953163a3a3aa033710b691c4bc3
Amarda Cullhaj <amardacullhaj@Amardas-MacBook-Pro.local>
2012-06-28 09:35:47 +0200

    exit entity hinzugefuegt

---
+++ Bomberman/src/Entities/ExitEntity.java
@@ -0,0 +1,19 @@
+package Entities;
+
+
+public class ExitEntity extends Entity
+ {
+
+       public ExitEntity(String sprite_name, int x, int y)
+     {
+           super(sprite_name, x, y);
+          // TODO Auto-generated constructor stub
+     }
+
+     @Override
+     public void collidedWith(Entity other) {
+     // TODO Auto-generated method stub
+
+     }
+
+}
